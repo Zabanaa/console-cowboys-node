b446a0354d4986fecd3cd3e61f70ae196cc5eaf6
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-10-24 09:49:20 +0000

    handle post requests

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port      = process.env.PORT || 3000
 const bodyParser = require("body-parser")
 const mongoose  = require("mongoose")
 const Job       = require("./models/jobs")
+const helpers   = require("./helpers")
 
 mongoose.connect("mongodb://localhost/console_cowboys")
 
@@ -26,12 +27,67 @@ db.once("open", () => console.log("Connected !") )
 // Check DB Error
 db.on("error", err => console.log(err) )
 
-app.get("/", (req, res) => {
+app.get("/jobs", (req, res) => {
 
     let jobs = Job.find({}, (err, jobs) => {
         if (err) console.log(err)
         return res.status(200).json(jobs)
     })
+})
+
+app.post("/jobs", (req, res) => {
+
+    let body = req.body
+    let job  = new Job()
+
+    job.title           = req.body.title
+    job.location        = req.body.location
+    job.contract_type   = req.body.contract_type
+    job.company_name    = req.body.company_name
+    job.listing_url     = req.body.listing_url
+
+    job.save()
+        .then( job => {
+            return res.status(201).json({
+                "type": "success",
+                "data": job
+            })
+        })
+        .catch( err => {
+
+            let errorMessage = err.message
+
+            if (err.message.includes("duplicate key")) {
+                return res.status(409).json({
+                    "type": "error",
+                    "message": "This listing URL already exists"
+                })
+            }
+            else if (err.message.includes("enum")) {
+                let fields = helpers.extractFields(err.message)
+                console.log("Fields", fields)
+                return res.status(422).json({
+                    type: "error",
+                    message: `${fields[0]} is not a valid ${fields[1]}`,
+                    fields: fields,
+                })
+            }
+            else if (err.message.includes("is required")) {
+                let fields = helpers.extractFields(err.message)
+                return res.status(422).json({
+                    "type": "error",
+                    "message": "Some fields are missing",
+                    "missingFields": fields
+                })
+            }
+            else {
+                return res.status(500).json({
+                    "type": "error",
+                    "message": "Something happened server side",
+                    "error": err.message
+                })
+            }
+        })
 })
 
 app.listen(port, () => {
